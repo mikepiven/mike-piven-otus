273f00d8193fe369cb78813ed0067830606f3386
Default Profile <Default Profile>
2021-07-04 22:43:21 +0300

    JavaScript-1

---
+++ javascript/1-association-items/src/index.js
@@ -0,0 +1,5 @@
+import { maxItemAssociation } from './maxItemAssociation.js'
+
+const payload = [["a", "b"],["a", "c"],["d", "e"]]
+
+console.log('maxItemAssociation :>> ', maxItemAssociation(payload))
